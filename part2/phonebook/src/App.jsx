5d8e125166a6cf192b9b129c9fc9aa3971860e1b
Ronaldo Almirón <rony.almiron.015@gmail.com>
2025-01-30 11:07:53 -0300

    Create exercises 2.11. part2

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,14 +2,11 @@ import { useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import { useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [search, setSearch] = useState('')
@@ -21,6 +18,13 @@ const App = () => {
     if (found) return alert(`${value} is already added to phonebook`)
     setNewName(value)
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
+
 
   const handleChangeNumber = (event) => {
     setNewNumber(event.target.value)
