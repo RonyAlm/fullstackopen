791edb87b1cb28ad8386cd1c769d57317814850c
Ronaldo Almirón <rony.almiron.015@gmail.com>
2025-07-24 08:22:59 -0300

    Agregar soporte para CORS y configurar el puerto desde la variable de entorno

--- part3/phonebook-api/index.js
+++ part3/phonebook-api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 morgan.token('body', (req, res) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : '';
@@ -75,7 +77,7 @@ app.post('/api/persons', (req, res) => {
     res.status(201).json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
 });
